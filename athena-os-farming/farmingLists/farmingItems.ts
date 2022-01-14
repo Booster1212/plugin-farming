9e5b4c201da5f61f42d9305e2e5faef37e9c1f4e
Locutus1119 <91572761+Locutus1119@users.noreply.github.com>
2022-01-15 00:14:37 +0100

    Farming item
    Grape, Orange, Pepper, Potato, Salad, Tomato, Wood.

--- athena-os-farming/farmingLists/farmingItems.ts
+++ athena-os-farming/farmingLists/farmingItems.ts
@@ -63,6 +63,114 @@ export const farmingItems: Array<Item> = [
         dbName: 'OS-Farming-Titanium-Ore',
         version: 1 
     },
+    {
+        name: 'Oak Wood',
+        icon: 'crate',
+        description: 'Some example description.',
+        behavior: ITEM_TYPE.CAN_DROP | ITEM_TYPE.CAN_STACK | ITEM_TYPE.CAN_TRADE, 
+        quantity: 1,
+        data: {
+            rarity: 'common',
+        },
+        dbName: 'OS-Farming-Oak-Wood',
+        version: 1 
+    },
+    {
+        name: 'Rotten Wood',
+        icon: 'crate',
+        description: 'Some example description.',
+        behavior: ITEM_TYPE.CAN_DROP | ITEM_TYPE.CAN_STACK | ITEM_TYPE.CAN_TRADE, 
+        quantity: 1,
+        data: {
+            rarity: 'common',
+        },
+        dbName: 'OS-Farming-Rotten-Wood',
+        version: 1 
+    },
+    {
+        name: 'Tomato',
+        icon: 'crate',
+        description: 'Some example description.',
+        behavior: ITEM_TYPE.CAN_DROP | ITEM_TYPE.CAN_STACK | ITEM_TYPE.CAN_TRADE, 
+        quantity: 1,
+        data: {
+            rarity: 'common',
+        },
+        dbName: 'OS-Farming-Tomato',
+        version: 1 
+    },
+    {
+        name: 'Orange',
+        icon: 'crate',
+        description: 'Some example description.',
+        behavior: ITEM_TYPE.CAN_DROP | ITEM_TYPE.CAN_STACK | ITEM_TYPE.CAN_TRADE, 
+        quantity: 1,
+        data: {
+            rarity: 'common',
+        },
+        dbName: 'OS-Farming-Orange',
+        version: 1 
+    },
+    {
+        name: 'Salad',
+        icon: 'crate',
+        description: 'Some example description.',
+        behavior: ITEM_TYPE.CAN_DROP | ITEM_TYPE.CAN_STACK | ITEM_TYPE.CAN_TRADE, 
+        quantity: 1,
+        data: {
+            rarity: 'common',
+        },
+        dbName: 'OS-Farming-Salad',
+        version: 1 
+    },
+    {
+        name: 'Potate',
+        icon: 'crate',
+        description: 'Some example description.',
+        behavior: ITEM_TYPE.CAN_DROP | ITEM_TYPE.CAN_STACK | ITEM_TYPE.CAN_TRADE, 
+        quantity: 1,
+        data: {
+            rarity: 'common',
+        },
+        dbName: 'OS-Farming-Potate',
+        version: 1 
+    },
+    {
+        name: '',
+        icon: 'crate',
+        description: 'Some example description.',
+        behavior: ITEM_TYPE.CAN_DROP | ITEM_TYPE.CAN_STACK | ITEM_TYPE.CAN_TRADE, 
+        quantity: 1,
+        data: {
+            rarity: 'common',
+        },
+        dbName: 'OS-Farming-',
+        version: 1 
+    },
+    {
+        name: 'Pepper',
+        icon: 'crate',
+        description: 'Some example description.',
+        behavior: ITEM_TYPE.CAN_DROP | ITEM_TYPE.CAN_STACK | ITEM_TYPE.CAN_TRADE, 
+        quantity: 1,
+        data: {
+            rarity: 'common',
+        },
+        dbName: 'OS-Farming-Pepper',
+        version: 1 
+    },
+    {
+        name: 'Grape',
+        icon: 'crate',
+        description: 'Some example description.',
+        behavior: ITEM_TYPE.CAN_DROP | ITEM_TYPE.CAN_STACK | ITEM_TYPE.CAN_TRADE, 
+        quantity: 1,
+        data: {
+            rarity: 'common',
+        },
+        dbName: 'OS-Farming-Grape',
+        version: 1 
+    },
 ]
 
 for(let x = 0; x < farmingItems.length; x++) {
